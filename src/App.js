44743220659ce7d6b2bd730b9dabf89ac0fa357f
akshay <akshaypvkeerakkal@gmail.com>
2022-03-03 17:26:46 +0530

    jsx and css added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,30 @@ import './App.css';
 
 function App() {
   return (
-    <h1>To do list</h1>
+    <div className="app">
+    <div className="mainHeading">
+      <h1>ToDo List</h1>
+    </div>
+    <div className="subHeading">
+      <br />
+      <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+    </div>
+    <div className="input">
+      <input type="text" placeholder="🖊️ Add item..." />
+      <i className="fas fa-plus"></i>
+    </div>
+    <div className="todos">
+      <div className="todo">
+        <div className="left">
+          <input type="checkbox" name="" id="" />
+          <p>Rect tutorial</p>
+        </div>
+        <div className="right">
+          <i className="fas fa-times"></i>
+        </div>
+      </div>
+    </div>
+  </div>
   );
 }
 
